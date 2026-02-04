dfd0a27f3eee61f41f5d8d16ab74ab0a626cb001
Cdavidjara <christian20david@hotmail.com>
2026-02-04 00:10:50 -0500

    Soporte PostgreSQL para despliegue en Render

--- app/Infrastructure/Database/MySQLConnection.php
+++ app/Infrastructure/Database/MySQLConnection.php
@@ -10,8 +10,14 @@ class MySQLConnection
         $config = require __DIR__ . '/../../../config/config.php';
         $db = $config['db'];
 
+        if ($db['driver'] === 'pgsql') {
+            $dsn = "pgsql:host={$db['host']};port={$db['port']};dbname={$db['name']}";
+        } else {
+            $dsn = "mysql:host={$db['host']};port={$db['port']};dbname={$db['name']};charset=utf8";
+        }
+
         $this->connection = new PDO(
-            "mysql:host={$db['host']};dbname={$db['name']};charset=utf8",
+            $dsn,
             $db['user'],
             $db['pass'],
             [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
--- config/config.php
+++ config/config.php
@@ -1,11 +1,11 @@
 <?php
-
 return [
     'db' => [
-        'host' => 'localhost',
-        'port' => '3306',
-        'name' => 'taxi_app',
-        'user' => 'root',
-        'pass' => '' // XAMPP por defecto
+        'driver' => getenv('DB_DRIVER') ?: 'mysql',
+        'host'   => getenv('DB_HOST') ?: 'localhost',
+        'port'   => getenv('DB_PORT') ?: '3306',
+        'name'   => getenv('DB_NAME') ?: 'taxi_app',
+        'user'   => getenv('DB_USER') ?: 'root',
+        'pass'   => getenv('DB_PASS') ?: ''
     ]
 ];
